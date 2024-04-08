09e1e69eb73bc6f03aab54cd083b8a4556690dd0
Mags8934 <maggie.werning@gmail.com>
2024-04-08 03:36:01 -0500

    rebuild wip

--- ecm6/src/app/app.routes.ts
+++ ecm6/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BuyAHorseComponent } from './buy-a-horse/buy-a-horse.component';
+import { SellYourHorseComponent } from './sell-your-horse/sell-your-horse.component';
+import { OtherServicesComponent } from './other-services/other-services.component';
 
 export const routes: Routes = [
-
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent},
-
+    { path: 'contact', component: ContactComponent},
+    { path: 'buy-a-horse', component: BuyAHorseComponent },
+    { path: 'sell-your-horse', component: SellYourHorseComponent },
+    { path: 'other-services', component: OtherServicesComponent },
+    { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
